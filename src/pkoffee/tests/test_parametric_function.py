82987bce9e7b9825bb8e896837357868bdbc3344
Filip J. Baran <filip.j.baran@uj.edu.pl>
2026-01-16 18:18:01 +0100

    Simple test for parametric_function added

---
+++ src/pkoffee/tests/test_parametric_function.py
@@ -0,0 +1,47 @@
+import pytest
+import numpy as np
+
+def test_quadratic():
+    from pkoffee.parametric_function import Quadratic
+    
+    rng = np.random.default_rng(seed=0)
+    [x, a0, a1, a2] = rng.normal(size=4)
+    func = Quadratic()
+
+    np.testing.assert_equal(func(x, a0, a1, a2), a0 + a1 * x + a2 * x**2)
+
+def test_MichaelisMentenSaturation():
+    from pkoffee.parametric_function import MichaelisMentenSaturation
+    
+    rng = np.random.default_rng(seed=0)
+    [x, v_max, k, y0] = rng.normal(size=4)
+    func = MichaelisMentenSaturation()
+
+    np.testing.assert_equal(func(x, v_max, k, y0), y0 + v_max * (x / np.maximum(k + x, 1e-9)))
+
+def test_Logistic():
+    from pkoffee.parametric_function import Logistic
+    
+    rng = np.random.default_rng(seed=0)
+    [x, L, k, x0, y0] = rng.normal(size=5)
+    func = Logistic()
+
+    np.testing.assert_equal(func(x, L, k, x0, y0), y0 + L / (1.0 + np.exp(-k * (x - x0))))
+
+def test_PeakModel():
+    from pkoffee.parametric_function import PeakModel
+    
+    rng = np.random.default_rng(seed=0)
+    [x, a, b] = rng.normal(size=3)
+    func = PeakModel()
+
+    np.testing.assert_equal(func(x, a, b), a * x * np.exp(-x / np.maximum(b, 1e-9)))
+
+def test_Peak2Model():
+    from pkoffee.parametric_function import Peak2Model
+    
+    rng = np.random.default_rng(seed=0)
+    [x, a, b] = rng.normal(size=3)
+    func = Peak2Model()
+
+    np.testing.assert_equal(func(x, a, b), a * (x**2) * np.exp(-x / np.maximum(b, 1e-9)))
